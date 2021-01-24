40804424a560adb0b4d4e407ead898c92feac700
muhhylmi <hylmi.muh@gmail.com>
2021-01-24 17:27:06 +0700

    berhasil membuat login

--- middleware/auth.js
+++ middleware/auth.js
@@ -41,3 +41,53 @@ exports.registrasi = function (req, res) {
     }
   });
 };
+
+// controller untuk login
+exports.login = function (req, res) {
+  var post = {
+    password: req.body.password,
+    email: req.body.email,
+  };
+  var query = "select * from ?? where ??=? AND ??=?";
+  var table = ["user", "password", md5(post.password), "email", post.email];
+
+  query = mysql.format(query, table);
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 1) {
+        var token = jwt.sign({ rows }, config.secret, {
+          expiresIn: 1440,
+        });
+        id_user = rows[0].user_id;
+        var data = {
+          id_user: id_user,
+          access_token: token,
+          ip_address: ip.address(),
+        };
+        var query = "insert into ?? set ?";
+        var table = ["akses_token"];
+
+        query = mysql.format(query, table);
+        connection.query(query, data, function (error, rows) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.json({
+              success: true,
+              message: "token JWT tergenerate",
+              token: token,
+              currUser: data.id_user,
+            });
+          }
+        });
+      } else {
+        res.json({
+          error: true,
+          message: "email / password salah",
+        });
+      }
+    }
+  });
+};
