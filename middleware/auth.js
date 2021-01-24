d2e79c39c3bc7ef5187392745c204fa2c60e489c
muhhylmi <hylmi.muh@gmail.com>
2021-01-24 16:08:12 +0700

    install md5 dan menambah auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,43 @@
+var connection = require("../koneksi");
+var mysql = require("mysql");
+var md5 = require("md5");
+var response = require("../res");
+var jwt = require("jsonwebtoken");
+var config = require("../config/secret");
+var ip = require("ip");
+
+// controller untuk register
+exports.registrasi = function (req, res) {
+  var post = {
+    username: req.body.username,
+    email: req.body.email,
+    password: md5(req.body.password),
+    role: req.body.role,
+    tanggal_daftar: new Date(),
+  };
+  var query = "select email from ?? where ??";
+  var table = ["user", "email", post.email];
+
+  query = mysql.format(query, table);
+
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 0) {
+        var query = "insert into ?? set ??";
+        var table = ["user"];
+        query = mysql.format(query, table);
+        connection.query(query, function (error, rows) {
+          if (error) {
+            console.log(error);
+          } else {
+            response.ok("Berhasil Menambahkan User Baru");
+          }
+        });
+      } else {
+        response.ok("email sudah terdaftar!");
+      }
+    }
+  });
+};
