7d627e70bb5ca374b233cabd5c0c8b025fcd65c3
muhhylmi <hylmi.muh@gmail.com>
2021-01-24 10:44:19 +0700

    berhasil menampilkan database

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require("./koneksi");
 exports.index = function (req, res) {
   response.ok("Aplikasi Rest Api jalan", res);
 };
+
+// menampilkan semua mahasiswa
+exports.tampilDataSemua = function (req, res) {
+  connection.query("select * from mahasiswa", function (error, rows, fileds) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
