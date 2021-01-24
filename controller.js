d5b402bf4bac1be491e3d07074fa2838944a0d00
muhhylmi <hylmi.muh@gmail.com>
2021-01-24 12:05:49 +0700

    update dan hapus data

--- controller.js
+++ controller.js
@@ -50,3 +50,39 @@ exports.tambahDataMahasiswa = function (req, res) {
     }
   );
 };
+
+// mengubah data berdasarkan id
+
+exports.ubahDataMahasiswa = function (req, res) {
+  var id = req.body.id_mahasiswa;
+  var nim = req.body.nim;
+  var nama = req.body.nama;
+  var jurusan = req.body.jurusan;
+
+  connection.query(
+    "update mahasiswa set nim =?, nama=?, jurusan=? where id_mahasiswa=?",
+    [nim, nama, jurusan, id],
+    function (error, rows, fileds) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil Mengubah Data", res);
+      }
+    }
+  );
+};
+
+exports.deleteMahasiswa = function (req, res) {
+  var id = req.body.id_mahasiswa;
+  connection.query(
+    "delete from mahasiswa where id_mahasiswa=?",
+    [id],
+    function (error, rows, fileds) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil Hapus Data", res);
+      }
+    }
+  );
+};
--- routes.js
+++ routes.js
@@ -8,4 +8,6 @@ module.exports = function (app) {
   app.route("/tampil").get(jsonku.tampilDataSemua);
   app.route("/tampil/:id").get(jsonku.tampilBerdasarId);
   app.route("/tambah").post(jsonku.tambahDataMahasiswa);
+  app.route("/ubah").put(jsonku.ubahDataMahasiswa);
+  app.route("/hapus").delete(jsonku.deleteMahasiswa);
 };
